d4f517ddcaa78097fd620dbc971bd69ddd6560a3
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-02 20:29:54 +0200

    author tags

--- py/singleton.py
+++ py/singleton.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
+#
+# @author: Lothar Rubusch
+# @email: L.Rubusch@gmx.ch
+# @license: GPLv3
+# @2013-May-01
 
 class Singleton(object):
     _instance = None
