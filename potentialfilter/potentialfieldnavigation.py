529879d88348009db4fcba7ae460ab77ee16fd7e
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-02 20:36:34 +0200

    cleanup

--- potentialfilter/potentialfieldnavigation.py
+++ potentialfilter/potentialfieldnavigation.py
@@ -47,48 +47,49 @@ def solve( M ):
 
     
 ## START                                                                        
-    
-XMAX=60
-YMAX=40
-M[YMAX][XMAX]
-GOAL=(40, 60)
-START=(0, 0)
+if __name__ == '__main__':
+    XMAX=60
+    YMAX=40
+    M[YMAX][XMAX]
+    GOAL=(40, 60)
+    START=(0, 0)
 
-## repulsive potential field
-M = repulsive_potential_field( M )
-M = normalize( M )
-# TODO display
+    ## repulsive potential field
+    M = repulsive_potential_field( M )
+    M = normalize( M )
+    # TODO display
 
-## attractive potential field
-M = attractive_potential_field( M )
-M = normalize( M )
-# TODO display
+    ## attractive potential field
+    M = attractive_potential_field( M )
+    M = normalize( M )
+    # TODO display
 
-## path solving
-# TODO
-## visualization
-# TODO
-    
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import matplotlib.pyplot as plt
-import numpy as np
+    ## path solving
+    # TODO
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-        linewidth=0, antialiased=False)
-ax.set_zlim(-1.01, 1.01)
+    ## visualization
+    # TODO
 
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
-fig.colorbar(surf, shrink=0.5, aspect=5)
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    from matplotlib.ticker import LinearLocator, FormatStrFormatter
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-plt.show()
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    ax.set_zlim(-1.01, 1.01)
+
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
