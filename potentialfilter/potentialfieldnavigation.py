cb235b706fdfbc88ffe3c627006f4f3e6eba0420
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-03 11:27:30 +0200

    repulsive potential field

--- potentialfilter/potentialfieldnavigation.py
+++ potentialfilter/potentialfieldnavigation.py
@@ -50,21 +50,18 @@ def distance( pktsrc, pktdst ):
 def normalize( M ):
     maxvals = []
     maxval = -1
-    ## find overall max
     for y in range( len(M) ):
         maxvals += [max([elem for elem in M[y]])]
     maxval = max( maxvals[:] )
-    ## divide each value by overall max
     try:
         for y in range( len(M) ):
             for x in range( len(M[0]) ):
                 M[y][x] = M[y][x] / maxval
-        return M
     except ZeroDivisionError:
         print "ERROR: maxval was zero: ", maxval
         sys.exit( -1 )
 
-
+## visualization of results
 def visualize( Z ):
     Y = np.arange(0,YMAX,1)
     X = np.arange(0,XMAX,1)
@@ -78,9 +75,7 @@ def visualize( Z ):
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
-
-
-## fcn obstacle
+## obstacle
 def obstacle( M, obs ):
     k_rep = 5
     rho_zero = 20
@@ -95,15 +90,15 @@ def obstacle( M, obs ):
                 U_rep = float(M[y][x]) + 0.5 * float(k_rep) * (1.0 / float(dobs) - 1.0 / float(rho_zero) )**2
                 if k_rep < U_rep: U_rep = k_rep
             M[y][x] = U_rep
-# FIXME
-# XXX
-    return M
 
-## fcn repulsive_potential_field
+## repulsive_potential_field
 def repulsive_potential_field( M ):
-    
-    M = obstacle( M, [5, 8] )
-    return M
+    obstcs = [[10,10],[10,20],[10,30],[10,40],  [20,10],[20,20],[20,30],[20,40], [15,35]]
+
+    for obs in obstcs:
+        obsy = obs[0]
+        obsx = obs[1]
+        obstacle( M, [ [obsy,obsx-1], [obsy-1,obsx], [obsy,obsx], [obsy+1,obsx], [obsy,obsx+1] ] )
 
 
 ## attractive_potential_field
@@ -147,22 +142,22 @@ if __name__ == '__main__':
 
 
     ## attractive potential field
-    M = attractive_potential_field( M )
-    M = normalize( M )
-
-    ## visualize
-    visualize( M )
-    sys.exit(0)                   
-    ## repulsing potential field
-#    repulsive_potential_field( M )
+#    M = attractive_potential_field( M )
 #    M = normalize( M )
+#    visualize( M )
 
-    sys.exit(0)                   
+
+
+    ## repulsing potential field
+#    M = repulsive_potential_field( M )
+    repulsive_potential_field( M )
+#    M = normalize( M )
+    normalize( M )
 
     ## visualize
     visualize( M )
 
-
+    sys.exit(0)                   
 #    DB_print( M )  
 
 
