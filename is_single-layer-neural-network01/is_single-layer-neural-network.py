24780efe4eeeb15e1dff838f8ce277780f7bad07
Lothar Rubusch <l.rubusch@gmx.ch>
2013-09-28 18:30:03 +0200

    intelligent systems assignment 01 - single layer neural network

---
+++ is_single-layer-neural-network01/is_single-layer-neural-network.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# Code to train a single-layer neural network
+# Assignment01 for Intelligent Systems
+#
+# @author: Lothar Rubusch
+# @email: L.Rubusch@gmx.ch
+# @license: GPLv3
+# @2013-Sep-27
+
+
+class Node(object):
+    _value = "0"
+
+    def __new__(self )
+
+###
+if __name__ == '__main__':
+    print "READY."
+
