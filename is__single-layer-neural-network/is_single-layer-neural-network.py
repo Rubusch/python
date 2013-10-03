e29459256eaf92dbddf35e03d5e39e60590ba2f1
Lothar Rubusch <l.rubusch@gmx.ch>
2013-10-03 22:24:50 +0200

    IS single perceptron, start all over

--- is__single-layer-neural-network/is_single-layer-neural-network.py
+++ is__single-layer-neural-network/is_single-layer-neural-network.py
@@ -25,6 +25,8 @@ import sys
 
 ## random number, random() and randrange()
 import random
+## e.g. use as random.randrange( -1000, 1000 ) / 1000 
+
 
 ## plotting library
 import matplotlib.pyplot as plt
@@ -34,128 +36,131 @@ def die( msg = "" ):
     print "FATAL: " + msg
     sys.exit( -1 )
 
-def dotty( nodes ):
-    print "digraph G{"
-    print "node [style=filled];"
-    print "edge [arrowhead=none,arrowtype=normal,arrowtail=dot];"
-    for node in nodes:
-        node.downDescend();
-    print "}"
-
 
 class Node(object):
-## attributes (for overview)
-    _name = "abc"
-    _up = []
-    _down = []
-    _edgeWeight = {}
+    _name = ""
+    _nextlist = []
+    _selftest = -1
 
-    _xval = "0"
-    _bias = None
-    _net = None
-
-    _dotColor = ""
-
-    def __init__( self, name, color="" ):
+    def __init__( self, name ):
         self._name = name
-        self._xval = random.randrange( 0, 100 ) / 100
-        self._up = []
-        self._down = []
-        self._edgeWeight = {}
-        self._dotColor = color
+        self._nextlist = [];
+        self._selftest = 0
 
     def __str__( self ):
         return self._name
 
-#    def __run__( self ):
-# TODO calculate propagation?
-#        pass  
+    def addNext( self, nextnode, weight ):
+        self._nextlist.append( [ nextnode, weight ] )
 
-## private
-    def _addNeighbor( self, dct, node, weight ):
-        if "bias" == str(node):
-            self._bias = node
-#        elif "net" == str(node):
-#            self._net = node
+    def sizeNextList( self ):
+        return len( self._nextlist )
+
+    def selftest( self, val ):
+        self._selftest += 1
+        if 0 == self.sizeNextList():
+            print self._selftest
         else:
-            dct.append( node )
-        self._edgeWeight.update({node:weight})
+            for nd in self._nextlist:
+                nd[0].selftest( self._selftest )
 
-## public
-    def upAdd( self, node, weight ):
-        self._addNeighbor( self._up, node, weight )
+    def forward( self, inval ):
+        print "TODO " + str(inval)
+        pass
 
-    def up( self ):
-        return self._up;
-
-    def downAdd( self, node, weight ):
-        self._addNeighbor( self._down, node, weight )
-
-    def down( self ):
-        return self._down;
-
-    def xval( self ):
-        return self._xval
-
-    def setXval( self, x ):
-# TODO in case here apply node function?
-        self._xval = x
+    def backward( self, inval ):
+        pass
 
 
 
+class Monitor( object ):
+    _inputnodes = []
+    _netnode = None
+    _trainingset = []
+    _trainingtargets = []
+    _stack = []
 
-#    def _getNewWeights( self, weight ):
-## algorithm
-#
-#    1) Perform the forwardpropagation phase for an input pattern and calculate
-#       the output error
-#    2) Change all weight values of each weight matrix using the formula
-#       weight(old) + learning rate * output error * output(neurons i) * output(neurons i+1) * ( 1 - output(neurons i+1) )
-#    3) Go to step 1
-#    4) The algorithm ends, if all output patterns match their target patterns
-#        return weight + learning_rate * output_error * output[idx] * output[idx+1] * (1 - output[i+1])
+    def __init__( self ):
+        self._netnode = Node( "net" )
+        self._inputnodes = []
+        self._trainingset = []
+        self._trainingtargets = []
+        self._stack = []
 
-    def _computeInput( self ):
-        for item in self.up():
-            w += self._edgeWeight( item ) * self._input( item ) # TODO input( item ), a dict for second input value  
+    def createNet( self ):
+        self._inputnodes.append( Node("bias") )
+        self._inputnodes.append( Node("x1") )
+        self._inputnodes.append( Node("x2") )
 
-    def forwardPropagation( self ):
-# TODO         
-        f = 0
-        w0 = 0
-        if self._bias:
-            w0 = self._edgeWeight.get( self._bias )
-        for item in self._down:
-            wi = self._edgeWeight.get(item)
-            xi = self._xval
-            f += wi * xi
-        f += w0
+        self._inputnodes[0].addNext( self._netnode, 2 )
+        self._inputnodes[1].addNext( self._netnode, 0.8 )
+        self._inputnodes[2].addNext( self._netnode, -0.5 )
 
-    def forwardDotty( self ):
-        if self._dotColor: print str(self) + "[ fillcolor=" + self._dotColor + " ]"
-        for item in self._down:
-## in case we need different upward settings
-#            try:
-#                item.up().index( self ) # contains an upward reference?
-#            except ValueError:
-#                TODO: implement here upward connections
-            print str(self) + " -> " + str(item) + " [ label=" + str(self._edgeWeight.get(item)) + " ];"
-            item.forwardDotty()
+        ## run selftest
+        cnt = 0
+        for nd in self._inputnodes:
+            cnt += 1
+            nd.selftest( 1 )
+        print [str(cnt)]
+
+## TODO
+#        t2=-1  
+#        class2x = [6, 7, 6, 10, 4]
+#        class2y = [10, 7, 11, 5, 11]
+
+        self._trainingset = []
+
+        self._trainingset.append( [[1, 1, 1, 1, 1, 1], [1, 6, 3, 4, 3, 1], [8, 2, 6, 4, 1, 6]])
+        self._trainingtargets.append(1)
+
+        self._trainingsset.append( [[1, 1, 1, 1, 1], [6, 7, 6, 10, 4], [10, 7, 11, 5, 11]])
+        self._trainingtargets.append(-1)
+
+    def train( self ):
+        for targetset in self._trainingset:
+            for idxVal in 0:len( targetset[0] ):
+                for idxNode in 0:len( self._inputnodes ):
+                    inputvals = targetset[idxNode]
+                    self._inputset[idxNode].forward( inputvals[idxVal])
 
 
 
+    def _push( self, nd ):
+        self._stack.append( nd )
 
+    def _pop( self ):
+        return self._stack.pop()
 
+    
+    def snapshot( self ):
+        ## set up nodes
+# TODO refac
+        bias = Node( "bias" )
+        net = Node( "net" )
+        x1 = Node( "X1", "red" )
+        x2 = Node( "X2", "blue" )
 
-#    def backwardDotty( self ):
+        
 # TODO
-#        pass    
-
-    def backwardPropagation( self ):
+        plt.plot( class1x, class1y, 'ro' )
 # TODO
-        pass    
+        plt.plot( class2x, class2y, 'bo' )
 
+        xAxisMax = max(class1x + class2x)+1
+        xAxisMin = min(class1x + class2x)-1
+        yAxisMax = max(class1y + class2y)+1
+        yAxisMin = min(class1y + class2y)-1
 
+        # separator
+#        plt.plot( [xAxisMin, xAxisMax], [yAxisMax, yAxisMin] )
+
+        # scope
+        plt.axis( [xAxisMin, xAxisMax, yAxisMin, yAxisMax] )
+
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.show()
+        
 
 
 
@@ -164,54 +169,29 @@ class Node(object):
 learning_rate = 0.02
 if __name__ == '__main__':
 
-    ## set up nodes
-    bias = Node( "bias" )
-    net = Node( "net" )
-    x1 = Node( "X1", "red" )
-    x2 = Node( "X2", "blue" )
+    monitor = Monitor()
+    monitor.createNet()
+    monitor.train()
 
-## training set
-    t1=1  
-    class1x = [ 1, 6, 3, 4, 3, 1 ]
-    class1y = [ 8, 2, 6, 4, 1, 6 ]
-    plt.plot( class1x, class1y, 'ro' )
-
-    t2=-1  
-    class2x = [6, 7, 6, 10, 4]
-    class2y = [10, 7, 11, 5, 11]
-    plt.plot( class2x, class2y, 'bo' )
-
-    xAxisMax = max(class1x + class2x)+1;
-    xAxisMin = min(class1x + class2x)-1;
-    yAxisMax = max(class1y + class2y)+1;
-    yAxisMin = min(class1y + class2y)-1;
-
-    # separator
-    plt.plot( [xAxisMin, xAxisMax], [yAxisMax, yAxisMin] )
-
-    # scope
-    plt.axis( [xAxisMin, xAxisMax, yAxisMin, yAxisMax] )
-
-    plt.ylabel('some numbers')
-    plt.show()
+    die( "STOP" )         
 
 
 
-
+# TODO rm
 ## dotty
-    # x1 -> net
-    x1.downAdd( net, 0.8 )
-    net.upAdd( x1, 0.8 )
+    # # x1 -> net
+    # x1.downAdd( net, 0.8 )
+    # net.upAdd( x1, 0.8 )
 
-    # x2 -> net
-    x2.downAdd( net, -0.5 )
-    net.upAdd( x2, -0.5 )
+    # # x2 -> net
+    # x2.downAdd( net, -0.5 )
+    # net.upAdd( x2, -0.5 )
 
-    # bias -> net
-    bias.downAdd( net, 2 )
-    net.upAdd( bias, 2 )
+    # # bias -> net
+    # bias.downAdd( net, 2 )
+    # net.upAdd( bias, 2 )
 
-    dotty( [x1, x2, bias] )
+    # dotty( [x1, x2, bias] )
 
     print "# READY."
 
