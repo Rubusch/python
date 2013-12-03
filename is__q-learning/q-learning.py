5015dbe1a8b6e65f1cf9b3eda6581912375b8b05
Lothar Rubusch <l.rubusch@gmx.ch>
2013-12-03 05:21:58 +0100

    A04, cleanup

--- is__q-learning/q-learning.py
+++ is__q-learning/q-learning.py
@@ -300,35 +300,10 @@ class Agent(object):
                         zs.append( float(val) ) ## take default  by Position str()
                     xs.append(x)
                     ys.append(-y)
-                
-## plot_surface
-#        y = np.arange(-len(self._maze), 0.0, 1.0)
-#        x = np.arange(0.0, len(self._maze[0]), 1.0)
-#        X, Y = np.meshgrid(x,y)
-#        zs = np.array([z for z in zs])
-#        Z = zs.reshape(X.shape) 
-#        fig = plt.figure()
-#        ax = fig.add_subplot(111, projection='3d')
-#        ax.plot_surface(X, Y, Z)
-#        plt.show()
-
-        
-## delaunay
-#        from mayavi import mlab
-#        X = np.array(xs)
-#        Y = np.array(ys)
-#        Z = np.array(zs)
-#        pts = mlab.points3d(X,Y,Z,Z)
-#        mesh = mlab.pipeline.delaunay2d(pts)
-#        pts.remove()
-#        surf = mlab.pipeline.surface(mesh)
-#        mlab.show()
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(xs,ys,zs,s=50,c="#FFD700") # values, yellow
-#        ax.scatter(xs,ys,zs,s=50,c="#FF3300") # values, red
-#        ax.scatter(xs,ys,zs,s=50,c="#FF3300") # values, blue
 
         ax.scatter(xb,yb,zb,s=100,c="#838B8B") # boundary
 
@@ -389,6 +364,5 @@ if __name__ == '__main__':
     agent.print_maze()
 
     agent.DEBUG_plot(10)
-
 
     print "READY."
