d447098819dd1c552c12b9f9b976039493d4db21
Lothar Rubusch <l.rubusch@gmx.ch>
2013-12-08 01:19:16 +0100

    is, assignment 05 kickoff

---
+++ is__binary-genetic-algorithm/genetic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# Assignment05 for Intelligent Systems
+#
+# author: Lothar Rubusch
+# email: l.rubusch@gmx.ch
+
+# TODO 
+print "READY."
---
+++ is__rosenbrock-function-optimization/optimization.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# Assignment04 for Intelligent Systems
+#
+# author: Lothar Rubusch
+# email: l.rubusch@gmx.ch
+
+# TODO  
+print "READY."
