f6af80cd9255d5da2259568f1773c73daad57de2
Lothar Rubusch <l.rubusch@gmx.ch>
2014-05-20 19:04:41 +0200

    square and multiply, implemented

--- cryptoXXX__square_and_multiply_TODO/square_and_multiply.py
+++ cryptoXXX__square_and_multiply_TODO/square_and_multiply.py
@@ -27,8 +27,10 @@ only; start with 1^2*base for the first '1'
 in case apply at each step a mod(n) operation
 
 0b    1     1    0    1
-((( 1^2*4 )^2*4 )^2 )^2*4
+((( 1^2*4 )^2*4 )^2 )^2*4 = 67108864
 
+or with mod(123)
+((( 1^2*4 )^2*4 )^2 )^2*4 = 64 mod(123)
 
 source
 
@@ -45,31 +47,44 @@ def die(msg):
     sys.exit(1)
 
 def square_and_multiply(base, exp, modulo=0):
-    strexp = bin(exp)
-    print strexp
-    die("TODO") 
-# TODO
+    strexp = bin(exp)[2:]
+    res = 1
+    for char in strexp:
+        ## debug message
+        print "binary: %s..."%char
+        res = res*res
+        if 0 != modulo: res = res % modulo
+        if char == '1':
+            res = res*base
+            if 0 != modulo: res = res % modulo
+            ## debugging
+            print "\tidentified as '1', res = (res^2)*base = %d"%res
+        else:
+            print "\tidentified as '0': res = (res^2) = %d"%res
+
+    return res
+
 
 ### main ###
 def main(argv=sys.argv[1:]):
-    r0=27
-    r1=21
-    if 2 == len(argv):
+    base=4
+    exp=13
+    modulo=0
+    if 2 <= len(argv):
         if 0 < len(argv[0]) and 0 < len(argv[1]):
             try:
-                r0=int(argv[0])
-                r1=int(argv[1])
+                base=int(argv[0])
+                exp=int(argv[1])
+                if 3 == len(argv):
+                    if 0 < len(argv[2]): modulo=int(argv[2])
             except:
                 die("usage: %s <r0> <r1>\nOR call without arguments"%sys.argv[0])
 
-    if r0 <= r1: die("r0 must be greater than r1")
-
     ## get the greatest common divisor
-    print "%d"%gcd(r0, r1)
+    print "%d^%d mod(%d) = %d"%(base, exp, modulo, square_and_multiply(base, exp, modulo))
 
 
 ### start ###
 if __name__ == '__main__':
     main()
 print "READY.\n"
-
