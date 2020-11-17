28468cb2c5b95264dc86c30226ff47487a9e2e60
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-17 11:31:14 +0100

    python3: integer division

---
+++ python3/changes__integer-division/integer_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+##
+## changes from python2 to python3
+##
+## This change is particularly dangerous if you are porting code, or if you are
+## executing Python 3 code in Python 2, since the change in integer-division
+## behavior can often go unnoticed (it doesn’t raise a SyntaxError).
+
+
+
+
+import sys
+print('python version: ', sys.version)
+
+## \ - Division: in python3 better always use: 'float(3)/2' or '3/2.0' instead
+##     of a '3/2', and in python2 use a 'from __future__ import division'
+print('3 / 2 =', 3 / 2, '[1.5]')
+
+## \\ - Floor division: division that results into whole number adjusted to the
+##      left in the number line
+print('3 // 2 =', 3 // 2, '[1.0]')
+
+print('READY.')
