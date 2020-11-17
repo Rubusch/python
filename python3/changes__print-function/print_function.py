76b596287627bd3f32505611b4ce102d2db9a304
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-17 11:18:34 +0100

    python3: print function, continued

--- python3/changes__print-function/print_function.py
+++ python3/changes__print-function/print_function.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 ##
 ## changes from python2 to python3
+
 import sys
 
+## print arguments
 print('python version: ', sys.version)
 print('python version info: ', sys.version_info)
 
-## print w/o  LF / CR
-print('some text,', end='')
+## no  LF / CR
+print('some text,', end='') ## avoid EOL
 print(' some more text')
+
+## print several objects
+print('a', 'b')
 
 print('READY.')
