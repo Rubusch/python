958846fa5abcf9a2137ef306329855cce9dbbdaf
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-18 19:10:40 +0100

    python3: for - ported

---
+++ python3/for/for.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+arr = ['cat', 'mouse', 'monkey']
+print("...by element:")
+for x in arr:
+    print(x)
+
+print("...by index:")
+for x in range(len(arr)):
+    print(x, arr[x])
+
+print("READY.\n")
