b3ee7dc13bb96e569e2a335387abcb5ce5dc73ae
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-18 14:51:47 +0100

    subprocess: error handling

--- python3/subprocess/separate_call.py
+++ python3/subprocess/separate_call.py
@@ -10,7 +10,9 @@ try:
     #subprocess.call(["ls", "-l"])
 
     ## after python3.5
-    subprocess.run(["ls", "-la"])
+    res = subprocess.run(["ls", "-la"])
+	res.check_returncode() # in case raises exception
+	print('if we reach this, no exception was thrown...')
 except subprocess.CalledProcessError as e:
     print(e.output)
 
