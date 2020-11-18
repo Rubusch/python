edf4a6064742bc6e35d76b69803279a7be405dac
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-18 18:40:44 +0100

    timestamp: update

--- python3/time__timestamp/timestamp.py
+++ python3/time__timestamp/timestamp.py
@@ -6,8 +6,10 @@ print(datetime.datetime.now())
 
 # extract something for filenames
 ext = str( datetime.datetime.now() ).replace(' ', '-').replace(':','-').split('.')[0]
-
 print(ext)
+
+stamp = str( datetime.datetime.now() ).replace('-','').split(' ')[0]
+print(stamp)
 
 print("READY.\n")
 
