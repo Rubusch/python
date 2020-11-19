aab383511a635515b1cee3cca6947779eb214309
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-19 14:26:27 +0100

    python3: list__reduce - ported

---
+++ python3/list__reduce/reduce.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+## python3 needs this declaration for 'reduce()'
+from functools import reduce
+
+
+def add(x,y):
+    return x+y
+
+## usage: reduce(function, sequence)
+##
+## returns a single value constructed by calling the binary function "function"
+## on the first two items of the sequence, then on the result and the next item,
+## and so on
+print('simple reduce', reduce( add, range( 1, 11 )))
+
+## a third argument can be passed to indicate the starting value;
+## in this case the starting value is returned for an empty sequence,
+## and the function is first applied to the starting value and the
+## first sequence item, then to the result and the next item, and so on
+def sum(seq):
+    def add(x,y): return x+y
+    return reduce(add, seq, 0)
+
+print('third element reduce', sum( range(1, 11) ))
+
+print("READY.")
+
