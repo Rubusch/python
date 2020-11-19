4e557a4de36c3b0985eb7703f12ed5f1d42fdb19
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-19 12:57:12 +0100

    python3: file__remove-dir - ported

--- python3/file__remove-dir/removedir.py
+++ python3/file__remove-dir/removedir.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-#
-# @author: Lothar Rubusch
-# @email: L.Rubusch@gmx.ch
-# @license: GPLv3
-# @2013-May-01
+#!/usr/bin/python3
 
 import os
 import re
 
-
+## remove files and folders by provided name pattern
 def rmpattern( path, pattern ):
     pattern = re.compile( pattern )
     for each in os.listdir(path):
@@ -26,19 +20,26 @@ jack fell down and broke his crown
 and jill came tumbling after
 """
 
-if os.path.exists( './folder' ):
-    print "delete file and folder"
-    # remove "folder" in "./"
+foldername = './folder'
+
+if os.path.exists(foldername):
+    print("delete folder and content")
     regex = re.compile('folder')
     rmpattern( './', regex)
 
 else:
-    print "create file and folder"
-    # create "./folder" and "./folder/test.txt"
-    os.mkdir( './folder' )
-    FILE = open('./folder/test.txt', 'w')
+    print("create file and folder with content")
+    os.mkdir(foldername)
+    FILE = open(foldername + '/test.txt', 'w')
     FILE.write(text)
     FILE.close()
 
-print "READY.\n"
+
+## alternatively in python3 use shutil
+import shutil
+
+print('..and remove the folder again using shutil')
+shutil.rmtree(foldername, ignore_errors=True)
+
+print("READY.")
 
