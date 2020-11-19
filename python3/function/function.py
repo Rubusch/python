09bd7b2e058ac55fe51f19359872ec38e36bd52a
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-19 13:04:52 +0100

    python3: function - ported

--- python3/function/function.py
+++ python3/function/function.py
@@ -1,21 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-#
-# @author: Lothar Rubusch
-# @email: L.Rubusch@gmx.ch
-# @license: GPLv3
-# @2013-May-01
+#!/usr/bin/python3
 
 def fib(n):
     """print a fibonacci series up to n"""
     result = []
     a, b = 0, 1
     while b < n:
-        print b,
+        print(b,)
         result.append(b)
         a, b = b, a+b
     return result
 
 # call
 f2000 = fib(2000)
-print f2000, "READY.\n"
+print(f2000, "\nREADY.")
