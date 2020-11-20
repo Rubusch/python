aaf238eaec0f87b4ad84b6abeaf5ab7be9ca09ac
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-20 10:51:01 +0100

    file__replace-starting-with: update with regexp version

--- python3/file__replace-starting-with/find_replace.py
+++ python3/file__replace-starting-with/find_replace.py
@@ -6,16 +6,30 @@
 import sys
 import fileinput
 
-filename = 'test.txt'
+from shutil import copy
+
 pattern = 'jack '
 newtext = 'all work and no play makes jack a dull boy...'
 
-import shutil
-shutil.copy('test.txt.template', filename)
+
+## version using 'startswith()'
+filename = 'test__startswith.txt'
+copy('test.txt.template', filename)
 
 for line in fileinput.input([filename], inplace=True):
     if line.strip().startswith(pattern):
         line = newtext + '\n'
     sys.stdout.write(line)
+fileinput.close()
+
+
+## version using regexp
+filename = 'test__regexp.txt'
+copy('test.txt.template', filename)
+
+for line in fileinput.input([filename], inplace=True):
+    line = re.sub(r"^\s*"+pattern, newtext, line)
+    sys.stdout.write(line)
+fileinput.close()
 
 print('READY.')
