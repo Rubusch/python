d5d3cfcce5eec660b900cd3a43f6b302027ca23e
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-23 13:49:28 +0100

    convert: ported to python3

--- python2/number_2_string/convert.py
+++ python2/number_2_string/convert.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-#
-# @author: Lothar Rubusch
-# @email: L.Rubusch@gmx.ch
-# @license: GPLv3
-# @2013-May-01
+#!/usr/bin/python3
 
 number = 84
 string = 35
 
 ## convert to string
-print "string = '%s'"%str(number)
+print("string = '%s'"%str(number))
 
 ## convert to int
-print "number = '%d'"%int(string)
+print("number = '%d'"%int(string))
 
-print "READY."
+print("READY.")
