f7abab0042f6998b3d1d5b38d18302e42c126810
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-23 17:17:41 +0100

    subprocess__start-process: update

--- python3/subprocess__start-process/separate_call.py
+++ python3/subprocess__start-process/separate_call.py
@@ -9,9 +9,9 @@ try:
     #subprocess.call(["ls", "-l"])
 
     ## after python3.5
-    res = subprocess.run(["ls", "-la"])
-	res.check_returncode() # in case raises exception
-	print('if we reach this, no exception was thrown...')
+    res = subprocess.run(["ls", "-la"]) # alternatively use subprocess.run(shlex.split('ls -al'))
+    res.check_returncode() # in case raises exception
+    print('if we reach this, no exception was thrown...')
 except subprocess.CalledProcessError as e:
     print(e.output)
 
