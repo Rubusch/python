b1a50e66af0b0b3de1acec2e2a8fce6eea22688b
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-24 10:59:46 +0100

    import: import via python interpreter

---
+++ python3/import__module01/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+## easiest import strategy: Import a python module with python interpreter
+import module
+
+
+def main():
+    ## call imported function
+    module.func()
+    print('READY.')
+
+
+if __name__ == "__main__":
+    main()
