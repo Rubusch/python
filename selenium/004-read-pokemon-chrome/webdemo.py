4414c0416d19d3135e2f0fcdb51dc0acac143fb6
Lothar Rubusch <l.rubusch@gmail.com>
2023-03-23 19:12:36 +0000

    selenium: add some reading demos
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ selenium/004-read-pokemon-chrome/webdemo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+##
+## ref: https://www.zenrows.com/blog/firefox-headless#how-do-i-start-firefox-headless
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+url = "https://scrapeme.live/shop/"
+
+# google chrome approach
+options = webdriver.ChromeOptions()
+options.headless = True
+with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+	driver.get(url)
+	print("Page URL:", driver.current_url)
+	print("Page Title:", driver.title)
+
+	parent_elements = driver.find_elements(By.XPATH, "//a[@class='woocommerce-LoopProduct-link woocommerce-loop-product__link']")
+	for parent_element in parent_elements:
+		pokemon_name = parent_element.find_element(By.XPATH, ".//h2")
+		pokemon_link = parent_element.get_attribute("href")
+		pokemon_price = parent_element.find_element(By.XPATH, ".//span")
+
+		temporary_pokemons_data = {
+			"name": pokemon_name.text,
+			"link": pokemon_link,
+			"price": pokemon_price.text
+		}
+
+		print(temporary_pokemons_data)
